14a86b97121019fa6e5b17545406ca3fe657fb75
caishaodong <234248702@qq.com>
2016-02-01 11:45:41 +0800

    Changes to be committed: 	modified:   src/main/java/com/liefeng/property/service/ProprietorService.java 	modified:   src/main/resources/dubbo/dubbo-consumer.xml 	modified:   src/main/resources/logback.xml

--- src/main/java/com/liefeng/property/service/ProprietorService.java
+++ src/main/java/com/liefeng/property/service/ProprietorService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 
 import com.liefeng.base.vo.UserVo;
 import com.liefeng.core.dubbo.filter.ContextManager;
-import com.liefeng.intf.service.tcc.ITccMsgDubboService;
+import com.liefeng.intf.service.tcc.ITccMsgService;
 import com.liefeng.mq.type.TccBasicEvent;
 import com.liefeng.property.domain.household.ProprietorContext;
 import com.liefeng.property.vo.household.ProprietorVo;
@@ -22,14 +22,14 @@ public class ProprietorService {
 	private static Logger logger = LoggerFactory.getLogger(ProprietorService.class);
 	
 	@Autowired
-	private ITccMsgDubboService tccMsgDubboService;
+	private ITccMsgService tccMsgService;
 	
 	public void createProprietor(ProprietorVo proprietor) throws Exception{
 		ProprietorContext.build(proprietor).create();
 		ContextManager.getInstance().setOemCode(proprietor.getOemCode());
 		UserVo user = new UserVo();
 		user.setCreateTime(new Date());
-		tccMsgDubboService.sendTccMsg(TccBasicEvent.CREATE_CUSTOMER, user.toString());
+		tccMsgService.sendTccMsg(TccBasicEvent.CREATE_CUSTOMER, user.toString());
 	}
 	
 }
