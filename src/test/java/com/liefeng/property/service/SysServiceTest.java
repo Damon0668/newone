036ce082bfc2bb60dbc7dc8cb6e033a15975c6e3
Huangama <huangama.rr@gmail.com>
2016-03-03 09:29:42 +0800

    增加test环境配置文件

--- src/test/java/com/liefeng/property/service/SysServiceTest.java
+++ src/test/java/com/liefeng/property/service/SysServiceTest.java
@@ -9,6 +9,12 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.liefeng.Application;
+import com.liefeng.base.constant.UserConstants;
+import com.liefeng.base.vo.CustomerVo;
+import com.liefeng.base.vo.UserVo;
+import com.liefeng.core.dubbo.filter.ContextManager;
+import com.liefeng.core.exception.LiefengException;
+import com.liefeng.intf.base.ICheckService;
 import com.liefeng.intf.property.ISysService;
 import com.liefeng.property.constant.SysConstants;
 import com.liefeng.property.vo.sys.SysDictVo;
@@ -25,10 +31,43 @@ public class SysServiceTest {
 	@Autowired
 	private ISysService sysService;
 	
+	@Autowired
+	private ICheckService checkService;
+	
 	@Test
 	public void getDictByGroupCode() {
 		List<SysDictVo> dictList = 
 				sysService.getDictByGroupCode(SysConstants.DictGroup.FEE_TYPE);
 		System.out.println(dictList);
+	}
+	
+	@Test
+	public void exceptionTest() {
+		ContextManager.getInstance().setOemCode("property");
+		
+		UserVo userVo = new UserVo();
+		CustomerVo customerVo = new CustomerVo();
+		customerVo.setNational("汉族");
+		customerVo.setNativePlace("广东");
+		customerVo.setRealName("甄庭俊");
+		customerVo.setSex("1");
+		customerVo.setStatus("1");
+		
+		userVo.setEmail("zhentingjun@liefengtech.com");
+		userVo.setHouseholdType("1");
+		userVo.setMobile("13032045915");
+		userVo.setName("13032045915");
+		userVo.setPassword("111111");
+		userVo.setQq("306675799");
+		userVo.setRegisterType(UserConstants.RegisterType.PC);
+		
+		userVo.setCustomer(customerVo);
+		
+		try {
+			checkService.createUserCheck(userVo);
+		} catch (LiefengException e) {
+			System.out.println(e.getCode());
+			System.out.println(e.getMessage());
+		}
 	}
 }
