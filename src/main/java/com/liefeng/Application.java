c2936ce09059db93e9b282aa4b2f6e9b91c2fa19
caishaodong <234248702@qq.com>
2016-03-04 13:09:26 +0800

    修改提交错误

--- src/main/java/com/liefeng/Application.java
+++ src/main/java/com/liefeng/Application.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-//classpath:timetask.xml
-@ImportResource({"classpath:dubbo/dubbo.xml"})
+@ImportResource({"classpath:dubbo/dubbo.xml","classpath:timetask.xml"})
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
