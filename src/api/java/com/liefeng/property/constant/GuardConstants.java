3cdc2fa75993874589f7d6ad8e9309f71a8175ff
xuhuawen <979850818@qq.com>
2016-03-04 12:01:31 +0800

    门禁服务

---
+++ src/api/java/com/liefeng/property/constant/GuardConstants.java
@@ -0,0 +1,18 @@
+package com.liefeng.property.constant;
+
+/**
+ * 门禁相关常量
+ * @author 蔡少东
+ * @date 2016年3月3日
+ */
+public class GuardConstants {
+	
+	private GuardConstants(){
+		
+	}
+	
+	public static interface GuardCard{
+		
+
+	}
+}
