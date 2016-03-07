e12cc2f9d437cd871d8a5f147d309ad9821f5937
xuhuawen <979850818@qq.com>
2016-03-07 17:44:23 +0800

    通知app接口

---
+++ src/main/java/com/liefeng/property/controller/WorkbenchController.java
@@ -0,0 +1,52 @@
+package com.liefeng.property.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.liefeng.core.entity.DataListValue;
+import com.liefeng.core.entity.DataPageValue;
+import com.liefeng.intf.property.IWorkbenchService;
+import com.liefeng.property.vo.workbench.NoticeVo;
+
+/**
+ * 工作台服务类（app）
+ * @author xhw
+ * @2016年3月7日 下午3:54:17
+ */
+@RestController
+@RequestMapping(value = "/workbench")
+public class WorkbenchController {
+	private static Logger logger = LoggerFactory.getLogger(WorkbenchController.class);
+
+	@Autowired
+	private IWorkbenchService workbenchService;
+
+	/**
+	 * 查看已发布通知（分页、app）
+	 * @param terminal 接收端类型(1：电视；2：电脑；3：移动设备)
+	 * @param naticeType 通知类型（1：社区通告；2：温馨提醒；3：通知；4：社区动态）
+	 * @param projectId 项目id（员工：所管理的项目id字符串，业主：所在项目id）
+	 * @param groupId （员工：部门id，业主：楼栋id）
+	 * @param privilegeType 接收人类型（员工：1，业主：2）
+	 * @param page
+	 * @param size
+	 * @return
+	 * @author xhw
+	 * @2016年3月7日 下午3:58:45
+	 */
+	@RequestMapping("getNoticeList")
+	@ResponseBody
+	public DataListValue<NoticeVo> getNoticeList(String terminal, String noticeType, String projectId, String groupId, String privilegeType, Integer page, Integer size){
+		logger.info("terminal="+terminal+" noticeType="+noticeType+" projectId ="+projectId+"  groupId="+groupId+" privilegeType ="+privilegeType+"  page="+page+"  size ="+size);
+		
+		DataPageValue<NoticeVo> noticeVos = workbenchService.findNoticeOfPublished(terminal, noticeType, projectId, groupId, privilegeType, page, size);
+		logger.info("noticeVos ="+noticeVos);
+
+		return DataListValue.success(noticeVos.getDataList());
+	}
+
+}
