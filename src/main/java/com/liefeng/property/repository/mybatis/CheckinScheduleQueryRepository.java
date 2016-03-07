f49317c7cf2ded491d6f189eb8478509c5153e36
zhentingjun <zhentingjun@foxmail.com>
2016-03-07 21:34:54 +0800

    mybatis查询问价提交

---
+++ src/main/java/com/liefeng/property/repository/mybatis/CheckinScheduleQueryRepository.java
@@ -0,0 +1,14 @@
+package com.liefeng.property.repository.mybatis;
+
+import com.liefeng.core.mybatis.repository.BaseRepository;
+import com.liefeng.property.vo.household.CheckinScheduleVo;
+
+/**
+ * 入住时间安排Mybatis查询仓储层
+ * 
+ * @author ZhenTingJun
+ * @date 2016年3月7日
+ */
+public interface CheckinScheduleQueryRepository extends BaseRepository<CheckinScheduleVo> {
+	
+}
