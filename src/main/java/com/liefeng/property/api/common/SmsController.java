7639694ba6412995e42da46bee5325ee3b6342b1
caishaodong <234248702@qq.com>
2016-03-25 13:34:55 +0800

    优化

--- src/main/java/com/liefeng/property/api/common/SmsController.java
+++ src/main/java/com/liefeng/property/api/common/SmsController.java
@@ -1,5 +1,7 @@
 package com.liefeng.property.api.common;
 
+import java.util.Map;
+
 import javax.validation.Valid;
 
 import org.slf4j.Logger;
@@ -11,17 +13,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
+import com.liefeng.common.util.MyBeanUtil;
 import com.liefeng.core.entity.DataValue;
 import com.liefeng.core.entity.ReturnValue;
 import com.liefeng.intf.service.msg.ISmsService;
 import com.liefeng.property.api.ro.common.VerifyCodeRo;
 import com.liefeng.property.api.ro.common.VerifyRo;
+import com.liefeng.property.api.ro.finger.sms.SmsSendRo;
+import com.liefeng.service.vo.SMSMsgVo;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
 @Api(value="短信模块")
-@RestController("csmsController")
+@RestController
 @RequestMapping(value = "/api/common/sms")
 public class SmsController {
 	
@@ -43,6 +50,17 @@ public class SmsController {
 	@ResponseBody
 	public ReturnValue verifySmsCode(@Valid @ModelAttribute VerifyRo verifyRo){
 		smsService.verifySMSCode(verifyRo.getPhoneNum(), verifyRo.getAction(), verifyRo.getCode());
+		return ReturnValue.success();
+	}
+	
+	@ApiOperation(value="发送短信", notes="发送短信")
+	@RequestMapping(value="/send", method=RequestMethod.POST)
+	@ResponseBody
+	public ReturnValue send(@Valid @ModelAttribute SmsSendRo smsSend) throws Exception{
+		SMSMsgVo smsMsg = MyBeanUtil.createBean(smsSend, SMSMsgVo.class);
+		Map<String,String> data = JSON.parseObject(smsSend.getParamString(),new TypeReference<Map<String,String>>(){});
+		smsMsg.setData(data);
+		smsService.sendSMSMsg(smsMsg);
 		return ReturnValue.success();
 	}
 }
