9720fb8df4501d1b5102bb470193831145a50627
caishaodong <234248702@qq.com>
2016-04-20 13:49:53 +0800

    设备

--- src/main/java/com/liefeng/property/po/guard/GuardDevicePo.java
+++ src/main/java/com/liefeng/property/po/guard/GuardDevicePo.java
@@ -60,6 +60,11 @@ public class GuardDevicePo extends BaseValue {
 	private String guardNum;
 	
 	/**
+	 * 设备名称
+	 */
+	@Column(name = "guard_name")
+	private String guardName;
+	/**
 	 * 设备类型
 	 */
 	@Column(name = "guard_type")
@@ -211,5 +216,13 @@ public class GuardDevicePo extends BaseValue {
 
 	public void setGuardNum(String guardNum) {
 		this.guardNum = guardNum;
+	}
+
+	public String getGuardName() {
+		return guardName;
+	}
+
+	public void setGuardName(String guardName) {
+		this.guardName = guardName;
 	}
 }
