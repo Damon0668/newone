2f1cd62c9945abc661a59a3de830718c7568dce1
xuhuawen <979850818@qq.com>
2016-04-26 19:00:32 +0800

    房屋验收配置

---
+++ src/api/java/com/liefeng/property/vo/household/HouseCheckitemConfigVo.java
@@ -0,0 +1,90 @@
+package com.liefeng.property.vo.household;
+
+import com.liefeng.core.entity.BaseValue;
+
+/**
+ * 房屋验收项配置持久化对象
+ * @author xhw
+ * @date 2016年4月26日 下午4:19:44
+ */
+public class HouseCheckitemConfigVo extends BaseValue {
+
+	private static final long serialVersionUID = 3680938398990450847L;
+
+	/**
+	 * 主键
+	 */
+	private String id;
+	
+	/**
+	 * 父ID(从0开始) 
+	 */
+	private String parentId;
+	
+	/**
+	 * 项目名称
+	 */
+	private String itemname;
+	
+	/**
+	 * 排序
+	 */
+	private String sortindex;
+	
+	
+	/**
+	 * OEM编码
+	 */
+	private String oemCode;
+
+
+	public String getId() {
+		return id;
+	}
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+	public String getParentId() {
+		return parentId;
+	}
+
+
+	public void setParentId(String parentId) {
+		this.parentId = parentId;
+	}
+
+
+	public String getItemname() {
+		return itemname;
+	}
+
+
+	public void setItemname(String itemname) {
+		this.itemname = itemname;
+	}
+
+
+	public String getSortindex() {
+		return sortindex;
+	}
+
+
+	public void setSortindex(String sortindex) {
+		this.sortindex = sortindex;
+	}
+
+
+	public String getOemCode() {
+		return oemCode;
+	}
+
+
+	public void setOemCode(String oemCode) {
+		this.oemCode = oemCode;
+	}
+
+}
---
+++ src/main/java/com/liefeng/property/repository/mybatis/HouseCheckitemConfigQueryRepository.java
@@ -0,0 +1,13 @@
+package com.liefeng.property.repository.mybatis;
+
+import com.liefeng.core.mybatis.repository.BaseRepository;
+import com.liefeng.property.vo.household.HouseCheckitemConfigVo;
+
+/**
+ * 房屋验收项配置
+ * @author xhw
+ * @date 2016年4月26日 下午5:09:59
+ */
+public interface HouseCheckitemConfigQueryRepository extends BaseRepository<HouseCheckitemConfigVo>{
+	
+}
